7f3b5147bc6834ac156ec0b48a7d13a9efce3898
evanFullerRUNI <evan.fuller@post.runi.ac.il>
2024-02-08 00:46:34 +0200

    HW7 commit v1

--- SpellChecker.java
+++ SpellChecker.java
@@ -10,26 +10,77 @@ public class SpellChecker {
 		System.out.println(correction);
 	}
 
+	// Helper methods
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() == 0) {
+			return "";
+		}
+
+		return str.substring(1);
 	}
 
+	public static char head(String str) {
+		return str.charAt(0);
+	}
+	// end of helper methods
+
+	// recursive method for calculating Levenshtein distance between two strings
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		int distance = 0;
+		int length1 = word1.length();
+		int length2 = word2.length();
+
+		// if else statements for calculating Levenshtein distance based on formula given in HW7 PDF
+		if (length1 == 0) {
+			distance = length2;
+		} else if (length2 == 0) {
+			distance = length1;
+		} else if (head(word1) == head(word2)) {
+			distance = levenshtein(tail(word1), tail(word2));
+		} else {
+			distance = 1 + Math.min(levenshtein(tail(word1), word2),
+					Math.min(levenshtein(word1, tail(word2)), levenshtein(tail(word1), tail(word2))));
+		}
+		
+		return distance;
 	}
 
+	// read dictionary
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
-	}
+		int[] levenshteins = new int[dictionary.length];
+		int min = 0;
 
+		// calculate Levenshtein distance for each word in dictionary
+		for (int i = 0; i < dictionary.length; i++) {
+			levenshteins[i] = levenshtein(word, dictionary[i]);
+		}
+
+		// find minimum Levenshtein distance
+		for (int i = 0; i < dictionary.length; i++) {
+			if (levenshteins[i] < levenshteins[min]) {
+				min = i;
+			}
+		}
+
+		// if Levenshtein distance is less than threshold, return word
+		if (levenshteins[min] <= threshold) {
+			return dictionary[min];
+		}
+
+		return word;
+	}
 }
